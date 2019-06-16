0fbeb456104903cd112f4b190faba0c77c6dee73
1758380410@qq.com <1758380410@qq.com>
2019-06-16 21:47:07 +0800

    a

--- app.js
+++ app.js
@@ -1,30 +1,22 @@
 var utilMd5 = require('utils/md5.js');
 App({
 
-  /**
-   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
-   */
+  
   onLaunch: function () {
     
   },
 
-  /**
-   * 当小程序启动，或从后台进入前台显示，会触发 onShow
-   */
+  
   onShow: function (options) {
     //  console.log("show")
   },
 
-  /**
-   * 当小程序从前台进入后台，会触发 onHide
-   */
+  
   onHide: function () {
     // console.log("hide")
   },
 
-  /**
-   * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
-   */
+ 
   onError: function (msg) {
     // console.log("error")
   },
@@ -62,12 +54,12 @@ App({
     roomNum:0,
     acivityUrl:"",
     cardList:null,
-    card:null, // 会员卡信息
+    card:null, 
     sellMovielist:null,
     seat: null,
     moviesListDate: null,
-    offerDescription: null, // 充值优惠
-    ticketCoupons: null, // 可用影票优惠券
+    offerDescription: null, 
+    ticketCoupons: null, 
   },
   createMD5: function (apiname, nowtime) {
     var sign = utilMd5.hexMD5("HLBW2018SHAPPLET" + apiname + "SH076WZ80D98X5G2" + nowtime);
