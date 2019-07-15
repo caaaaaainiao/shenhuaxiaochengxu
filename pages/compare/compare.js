ae16f5be01dda7902329a93a66ca448c620e7627
sunzian <sunzian1@126.com>
2019-07-15 14:34:28 +0800

    af

--- pages/compare/compare.js
+++ pages/compare/compare.js
@@ -44,7 +44,7 @@ Page({
       swiperIndex: app.globalData.movieIndex
     })
     this.swiperIndex = this.data.swiperIndex
-    this.moviesList = app.globalData.moviearea
+    // this.moviesList = app.globalData.moviearea
     that.setData({
       movieId: app.globalData.movieId,
       moviearea: app.globalData.moviearea,
