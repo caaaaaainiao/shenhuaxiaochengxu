76abdca937dd921e39f209da64ad43e97c5fe97c
new <1443119959@qq.com>
2019-08-19 17:40:11 +0800

    0

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ App({
     Username: "MiniProgram",
     Password: "6BF477EBCC446F54E6512AFC0E976C41",
     // AppId: 'wx1baa1fc8240ef183', // 高美
-    // AppId: 'wxb491affbeb262f2f', // 国购
-    AppId: 'wx8eafe6ec48aa323f', // 越幕
+    AppId: 'wxb491affbeb262f2f', // 国购
+    // AppId: 'wx8eafe6ec48aa323f', // 越幕
     // AppId: 'wxa20eeb5bde481333', // 智泉
     // AppId: 'wx2069ef6c2d5b1880', // 美伦
     // AppId: 'wxfd577490c3d0a0a5', // 睢县
@@ -34,8 +34,8 @@ App({
     // AppId: 'wx8ed523bb8aa3b0b0', // 沃伦
     // AppId: 'wxb9c0ee0184070dc5', // 丰县
     // secret: '9461327783c15fe6c9cd421b0a6f9690', // 高美
-    // secret: '0e050ba2d7338a77d0988a3c5716f813', // 国购
-    secret: '6b5276609738d408c1640e37460a64e6', // 越幕
+    secret: '0e050ba2d7338a77d0988a3c5716f813', // 国购
+    // secret: '6b5276609738d408c1640e37460a64e6', // 越幕
     // secret: 'eff0e90de960e6cae166368c54de0b5f', // 智泉
     // secret: '145bc00fe2907c30ce35a166e50cfc89', // 美伦
     // secret: '6e4e81ce226bf638c6c9e82f49e41cb4', // 睢县
